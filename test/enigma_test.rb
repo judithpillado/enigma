e70804a78ddd49d410c9111f7763d9ff5f0ddf2f
Judith Pillado <judith.pillado@gmail.com>
2020-04-18 23:54:56 -0500

    Assert instance of enigma

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -2,4 +2,12 @@ require './test/test_helper'
 require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
-end 
+
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
+end
