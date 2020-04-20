b4a6fc9436a2ebd2ec22343fa682f63f37a1050a
Judith Pillado <judith.pillado@gmail.com>
2020-04-19 20:55:27 -0500

    Add test for randomizing numbers

--- test/key_test.rb
+++ test/key_test.rb
@@ -3,4 +3,16 @@ require './lib/key'
 
 class KeyTest < Minitest::Test
 
-end 
+  def setup
+    @key = Key.new
+  end
+
+  def test_it_exists
+    assert_instance_of Key, @key
+  end
+
+  def test_it_can_randomize_numbers
+    assert_equal 5, @key.randomize_numbers.length
+  end
+
+end
