8dcd76a554d5ead987e767bdcd121f98f066ed61
Judith Pillado <judith.pillado@gmail.com>
2020-04-19 20:37:33 -0500

    Add squared date test

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -3,4 +3,16 @@ require './lib/offset'
 
 class OffsetTest < Minitest::Test
 
+  def setup
+    @offset = Offset.new
+  end
+
+  def test_it_exists
+    assert_instance_of Offset, @offset
+  end
+
+  def test_squared_date
+    assert_equal 4, @offset.squared_date.length
+  end
+
 end
