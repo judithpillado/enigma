b80db24343fbfa9acb22cc783393a6f439fe7f20
Judith Pillado <judith.pillado@gmail.com>
2020-04-19 20:38:00 -0500

    Add squared date method

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,2 +1,17 @@
+require 'date'
+
 class Offset
-end 
+  attr_reader :date
+
+  def initialize
+    @date = Time.now.strftime("%m%d%y")
+  end
+
+  def squared_date
+    squared = @date.to_i * @date.to_i
+    strings = squared.to_s.chars[-4..-1]
+    offsets = strings.map { |string| string.to_i }
+    offsets
+  end
+
+end
